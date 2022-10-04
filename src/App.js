f62f98bc57817b52e898e402cfc997c769f79489
Che Van Thao <chevanthao96@gmail.com>
2022-10-05 01:25:52 +0900

    test

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import NarBar from './NarBar/NarBar.js'
 import ViewCategory from './ViewCategory/ViewCategory.js'
 import RecommentedSession from './RecommentedSession/RecommentedSession.js'
 import ContactSession from './ContactSession/ContactSession.js'
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/">
           <div className="App">
             <NarBar></NarBar>
             <ViewCategory></ViewCategory>
             <RecommentedSession></RecommentedSession>
             <ContactSession></ContactSession>
           </div>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    //     {/* </Route>
+    //   </Routes>
+    // </BrowserRouter> */}
   );
 }
 
---
+++ vite-project/src/routes/errorContacts.jsx
@@ -0,0 +1,5 @@
+export default function ErrorContacts() {
+    return (
+        <div><h1>this is a Error Contact 1</h1></div>
+    )
+}
