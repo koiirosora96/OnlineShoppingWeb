fe282682d87dfb8b88b848d51ce01ab4ec6b8a67
ThaoCV <che.van.thao@jam-tech.jp>
2022-10-13 18:25:47 +0900

    add animation to Search bar

--- src/NarBar/NarBarSearch.js
+++ src/NarBar/NarBarSearch.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function NarBarSearch() {
     return ( 
         <div className="NarBarSearch">
-            <Link to="/" style={{textDecoration:"none", color:"white"}}><h2>Search</h2></Link>
+            <Link to="/" style={{textDecoration:"none", color:"white"}}><span><h2>Search</h2></span></Link>
             <div className='NarBarSearch-input'>
                 <input type="text" placeholder='Search here'></input>
             </div>
