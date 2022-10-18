c76430472fe24fafffe9830f34e8534c644f32d3
ThaoCV <che.van.thao@jam-tech.jp>
2022-10-18 11:37:14 +0900

    fix screen 1

--- src/ViewCategory/Category.js
+++ src/ViewCategory/Category.js
@@ -1,11 +1,15 @@
 import "./category.css"
 import { Link } from 'react-router-dom'
 
-function Category() {
+function Category(props) {
+    console.log(props)
     return ( 
         <div className="category">
-            <Link to="/products" ><img src=""></img></Link>
-            <h2>Laptop</h2>
+            <Link to="/products" ><img style={{
+                backgroundImage: `url(${props.image})`
+                }}></img>
+            </Link>
+            <h2>{props.name}</h2>
         </div>
      );
 }
--- src/ViewCategory/ViewCategory.js
+++ src/ViewCategory/ViewCategory.js
@@ -2,13 +2,50 @@ import "./ViewCategory.css"
 import Category from "./Category.js"
 
 function ViewCategory() {
+    const SearchPageItems = [
+        {
+            "name": "Laptop",
+            "image": "src/SearchPagePics/Laptop.jpeg",
+            "category": "computer",
+            "title": "laptop",
+            "recommented": 1
+        },
+        {
+            "name": "Clothes",
+            "image": "src/SearchPagePics/Clothes.jpg",
+            "category": "goods",
+            "title": "Clothes",
+            "recommented": 1
+        },
+        {
+            "name": "Iphone Case",
+            "image": "src/SearchPagePics/IphoneCase.jpeg",
+            "category": "cellPhone",
+            "title": "IphoneCase",
+            "recommented": 0
+        },
+        {
+            "name": "LivingRoom",
+            "image": "src/SearchPagePics/LivingRoom.jpeg",
+            "category": "goods",
+            "title": "LivingRoom",
+            "recommented": 1
+        },
+        {
+            "name": "Shoes",
+            "image": "src/SearchPagePics/Shoes.jpeg",
+            "category": "goods",
+            "title": "Shoes",
+            "recommented": 1
+        },
+    ]
+
     return ( 
         <div className="ViewCategory">
-            <Category></Category>
-            <Category></Category>
-            <Category></Category>
-            <Category></Category>
-            <Category></Category>
+            {
+            SearchPageItems.map(item => {
+                return <Category name={item.name} image={item.image} ></Category>})
+            }
         </div>
      );
 }
